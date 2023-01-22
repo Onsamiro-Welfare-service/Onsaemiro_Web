7e40330404fa6022b45a8acaeb7eab34ffbfa540
김승주 <46523568+WWvision@users.noreply.github.com>
2023-01-22 23:59:29 +0900

    0122 업데이트
    사용자 요구사항 페이지 구현
    - LocalStorage이용하여 세션 데이터 저장 가능

--- static/Onsae_UserRequirement_default_logic.js
+++ static/Onsae_UserRequirement_default_logic.js
@@ -2,13 +2,19 @@ var UserList = [
     { User_Id: "UserId1111" ,Name: "김승주", birth: "0000-00-00", address: "주소1"}
 ];//사용자 정보 리스트
 var UserRequirementList = [//웹에 저장된 사용자 요구사항 리스트
-    // { id: "UserId1111", date: '01-10-18:32', is_photo: false, text: "질문내용 이거저거그것", photo: "src~~~~"},
-    // { id: "UserId2222", date: '01-10-18:32', is_photo: true, text: "질문내용 이거저거그것", photo: "../src/img/avatars/avatar-2.jpg"},
+    //{ id: "Error Code: 404 Not Found", date: "", is_photo: false, text: "사용자 요구사항 데이터를 정상적으로 불러오지 못했습니다", photo: "" }
+    { id: "UserId1111", date: '01-10-18:32', is_photo: false, text: "질문내용 이거저거그것", photo: "src~~~~" },
+    { id: "UserId2222", date: '01-10-18:32', is_photo: true, text: "질문내용 이거저거그것", photo: "../src/img/avatars/avatar-2.jpg" }
 ];
 var NewRequirementList = [//서버에서 불러온 사용자 요구사항 리스트
     // { id: "UserId1111", date: '01-10-18:32', is_photo: false, text: "질문내용 이거저거그것", photo: "src~~~~"}
 ];
+var UserRequirement_SessionArr = [//현재 세션 데이터 저장
+    // { id: "", time: "", status: "", icon: "", confirmBtn : false, completeBtn: false }
+];
+var UserRequirement_LoadedArr = [];//이전 세션 데이터 불러오면 여기에 저장 
 var Exp_Name = new RegExp("[가-힣]{2,4}");
+
 
 //창닫기 이벤트 감지& 기존 세션 데이터 저장
 window.addEventListener('beforeunload', (event) => {
@@ -19,9 +25,6 @@ window.addEventListener('beforeunload', (event) => {
     // Chrome에서는 returnValue 설정이 필요함
     event.returnValue = '';
 });
-
-
-if()
 
 //기본 웹에서 버튼 및 작동 관련 함수
 function toggleDiv(DivId){
@@ -54,6 +57,50 @@ function Requirement_Complete(DivId){
     document.getElementById(DivId + "_Status").innerText = "조치 완료";
     document.getElementById(DivId + "_body").style.borderBottom = "2px solid black";
 }
+
+//기존 세션 정보를 저장하고 다시 로드될때 불러오게끔 하기 위함
+function save_SessionData(){//화면에 보이는 요구사항 상태값을 모두 저장하는 함수 
+    let parent = document.getElementById("RequirementList");
+    let parent_Count = parent.children.length;
+
+    //현재 세션 데이터 모두 긁어오기
+    for(let p=0; p < parent_Count; p++){
+        UserRequirement_SessionArr.push(get_SessionData(p));   
+    }
+    //세션 데이터 로컬스토리지에 저장
+    localStorage.setItem("UserRequirement_SessionData",JSON.stringify(UserRequirement_SessionArr));
+    UserRequirement_SessionArr = [];//초기화
+}
+
+function load_SessionData(){//이전 데이터가 있다면 해당 정보 불러와 UserRequirement_SessionArr에 저장하기
+    let localData = localStorage.getItem("UserRequirement_SessionData");
+    UserRequirement_LoadedArr = JSON.parse(localData);
+}
+load_SessionData();
+
+function get_SessionData(index){//인덱스에 해당하는 해당 요구사항Div 상태값을 모두 긁어와 객체형태로 리턴
+    let getData = {};
+    getData.id = UserRequirementList[index].id;//사용자Id
+    getData.time = UserRequirementList[index].date;//생성시간
+    getData.status = document.getElementById("Requirement" + index + "_Status").innerText;//요구사항 상태
+    getData.icon = document.getElementById("Requirement" + index + "_Icon").style.borderColor;//요구사항 아이콘 색상
+    getData.confirmBtn = document.getElementById("Requirement" + index + "_ConfirmBtn").disabled;//확인완료 버튼 상태
+    getData.completeBtn = document.getElementById("Requirement" + index + "_CompleteBtn").disabled;//조치완료 버튼 상태
+    return getData;//객체반환
+}
+
+function Update_Session(index){//이전 세션 정보가 있다면 생성된 정보에 기존 정보를 덮어 씌우는 함수
+    if(index <= UserRequirement_LoadedArr.length){
+        document.getElementById("Requirement" + index + "_Status").innerText = UserRequirement_LoadedArr[index].status;
+        document.getElementById("Requirement" + index + "_Icon").style.borderColor = UserRequirement_LoadedArr[index].icon;
+        document.getElementById("Requirement" + index + "_ConfirmBtn").disabled = UserRequirement_LoadedArr[index].confirmBtn;
+        document.getElementById("Requirement" + index + "_CompleteBtn").disabled = UserRequirement_LoadedArr[index].completeBtn;
+        if(UserRequirement_LoadedArr[index].completeBtn) document.getElementById("Requirement" + index + "_body").style.borderBottom = "2px solid black";
+    }
+}
+
+
+
 
 //콘텐트 생성 및 데이터 관련 함수
 function Search_UserName(id){
@@ -155,12 +202,16 @@ function Create_User_requirement(Obj){//생성되는 항목에 값 넣기
             CompleteBtn.setAttribute('type', 'button');
             CompleteBtn.setAttribute('id', eval("'Requirement" + List_index + "_CompleteBtn'"));
             CompleteBtn.setAttribute('class', 'Requirement_btn_style');
-            CompleteBtn.setAttribute('style', 'background-color: palegreen; margin-left: -6px;');
+            CompleteBtn.setAttribute('style', 'background-color: palegreen; margin-left: -4px;');
             CompleteBtn.setAttribute('value', '조치 완료');
             CompleteBtn.setAttribute('onclick', eval("'Requirement_Complete(`Requirement" + List_index + "`)'"));
             Btn.appendChild(CompleteBtn);
         child.appendChild(Btn);
-    parent.appendChild(child);         
+    parent.appendChild(child);      
+    
+    if(UserRequirement_LoadedArr.length > 0){//생성된 요구사항에 이전 세션 정보가 있다면 적용
+        Update_Session(List_index);
+    }
 }
 
 if(document.getElementById("RequirementList").children.length == 0){//아무값도 없다면?
@@ -184,10 +235,11 @@ function Request_UserRequirement_Api(){//사용자 요구사항 리스트 받아
         http://ec2-43-201-19-40.ap-northeast-2.compute.amazonaws.com/api/get_request
         그냥 저장된 요구사항 다 불러짐
      */
-
-    
+    //UserRequirementList = 받아온 데이터 할당
+    //UserRequirementList.push()로 데이터 추가하지 말것! default값이 있기때문
+    console.log("Request_Api");
     Delete_New_requirement();//새로운 데이터 로드 시
 }
 
 
-setInterval(() => Request_UserRequirement_Api(), 180000);//3분마다 사용자 요구사항 받아오게끔
+setInterval(() => Request_UserRequirement_Api(), 60000);//1분마다 사용자 요구사항 받아오게끔
