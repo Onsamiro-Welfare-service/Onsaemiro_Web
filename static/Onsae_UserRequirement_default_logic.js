dca4513459f2fbc644419c8816603bd3b07f5524
김승주 <46523568+WWvision@users.noreply.github.com>
2023-01-22 17:08:30 +0900

    0122 백업
    근무투입하기전에 백업 개념

--- static/Onsae_UserRequirement_default_logic.js
+++ static/Onsae_UserRequirement_default_logic.js
@@ -1,13 +1,27 @@
 var UserList = [
-    { key: "userid", value:"username"}
+    { User_Id: "UserId1111" ,Name: "김승주", birth: "0000-00-00", address: "주소1"}
 ];//사용자 정보 리스트
 var UserRequirementList = [//웹에 저장된 사용자 요구사항 리스트
-    { id: "userid", date: '01-10-18:32', is_photo: false, text: "질문내용 이거저거그것", photo: "src~~~~"}
+    // { id: "UserId1111", date: '01-10-18:32', is_photo: false, text: "질문내용 이거저거그것", photo: "src~~~~"},
+    // { id: "UserId2222", date: '01-10-18:32', is_photo: true, text: "질문내용 이거저거그것", photo: "../src/img/avatars/avatar-2.jpg"},
 ];
 var NewRequirementList = [//서버에서 불러온 사용자 요구사항 리스트
-    { id: "userid", date: '01-10-18:32', is_photo: false, text: "질문내용 이거저거그것", photo: "src~~~~"}
+    // { id: "UserId1111", date: '01-10-18:32', is_photo: false, text: "질문내용 이거저거그것", photo: "src~~~~"}
 ];
 var Exp_Name = new RegExp("[가-힣]{2,4}");
+
+//창닫기 이벤트 감지& 기존 세션 데이터 저장
+window.addEventListener('beforeunload', (event) => {
+    //화면이 사라지기전 해당화면 상태값 모두 저장
+    save_SessionData();
+    // 표준에 따라 기본 동작 방지
+    event.preventDefault();
+    // Chrome에서는 returnValue 설정이 필요함
+    event.returnValue = '';
+});
+
+
+if()
 
 //기본 웹에서 버튼 및 작동 관련 함수
 function toggleDiv(DivId){
@@ -47,9 +61,9 @@ function Search_UserName(id){
     UserList = JSON.parse(getData);
     let is_find = "이름을 찾지 못했습니다";
     UserList.forEach((elem, index) => {
-        let FindVal = elem.key;
+        let FindVal = elem.User_Id;
         if(FindVal.indexOf(id) != -1){//값을 찾는다면 리턴
-            is_find = UserList[index].value;
+            is_find = UserList[index].Name;
         }
     });
     return is_find;//이름값을 찾는다면 "이름", 못찾는다면 "404" 리턴
@@ -93,7 +107,66 @@ function Create_User_requirement(Obj){//생성되는 항목에 값 넣기
             header.appendChild(Status);
         child.appendChild(header);
             
+        let body = document.createElement('div');
+        body.setAttribute('id', eval("'Requirement" + List_index + "_body'"));
+        body.setAttribute('class', 'Requirement_body_style');
+        
+            let Times = document.createElement('div');
+            let createdTime = document.createTextNode('생성시간: ' + Obj.date);
+            Times.setAttribute('id', eval("'Requirement" + List_index + "_createdTime'"));
+            Times.setAttribute('style', 'font-weight: bold; margin:8px;');   
+            Times.appendChild(createdTime);
+            body.appendChild(Times);
+            
+            if(Obj.is_photo){//있다면 true
+                body.setAttribute('style', 'display:none;height: 400px;');
+                let Img = document.createElement('div');
+                Img.setAttribute('id', eval("'Requirement" + List_index + "_Img'"));
+                Img.setAttribute('class', 'body_ImgDiv_style');
+                    let Img_src = document.createElement('img');
+                    Img_src.setAttribute('src', eval("'" + Obj.photo + "'"));
+                    Img_src.setAttribute('style', 'margin-top: 2%;');
+                    Img.appendChild(Img_src);
+                body.appendChild(Img);
+            } else {//없다면
+                body.setAttribute('style', 'display:none;height: 150px;');
+            }
+            
+            let Requirement_txt = document.createElement('div');
+            let Requirement_txt_str = document.createTextNode(Obj.text);
+            Requirement_txt.setAttribute('id', eval("'Requirement" + List_index + "_Txt'"));
+            Requirement_txt.setAttribute('class', 'body_txt_style');
+            Requirement_txt.appendChild(Requirement_txt_str);
+            body.appendChild(Requirement_txt);
+        child.appendChild(body);
 
+        let Btn = document.createElement('div');
+        Btn.setAttribute('id', eval("'Requirement" + List_index + "_Btn'"));
+        Btn.setAttribute('style', 'width: 990px; height: 50px; display: none;');
+            let ConfirmBtn = document.createElement('input');
+            ConfirmBtn.setAttribute('type', 'button');
+            ConfirmBtn.setAttribute('id', eval("'Requirement" + List_index + "_ConfirmBtn'"));
+            ConfirmBtn.setAttribute('class', 'Requirement_btn_style');
+            ConfirmBtn.setAttribute('style', 'background-color: palegoldenrod;');
+            ConfirmBtn.setAttribute('value', '확인 완료');
+            ConfirmBtn.setAttribute('onclick', eval("'Requirement_Confirm(`Requirement" + List_index + "`)'"));
+            Btn.appendChild(ConfirmBtn);
+            let CompleteBtn = document.createElement('input');
+            CompleteBtn.setAttribute('type', 'button');
+            CompleteBtn.setAttribute('id', eval("'Requirement" + List_index + "_CompleteBtn'"));
+            CompleteBtn.setAttribute('class', 'Requirement_btn_style');
+            CompleteBtn.setAttribute('style', 'background-color: palegreen; margin-left: -6px;');
+            CompleteBtn.setAttribute('value', '조치 완료');
+            CompleteBtn.setAttribute('onclick', eval("'Requirement_Complete(`Requirement" + List_index + "`)'"));
+            Btn.appendChild(CompleteBtn);
+        child.appendChild(Btn);
+    parent.appendChild(child);         
+}
+
+if(document.getElementById("RequirementList").children.length == 0){//아무값도 없다면?
+    for(let i=0; i < UserRequirementList.length; i++){
+        Create_User_requirement(UserRequirementList[i]);
+    }
 }
 
 function Delete_New_requirement(){//새로운 데이터 로드시 갱신
